eaf3007425c9600cdb757143dff16b076e744eda
Ryan Barry <ryan@nuclearice.com>
2024-03-15 18:45:41 -0700

    parse bencoded integers

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
-use serde_json;
 use std::env;
 
 // Available if you need it!
 // use serde_bencode
 
-#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    // If encoded_value starts with a digit, it's a string
+    let mut chars = encoded_value.chars();
+    match chars.next() {
+        Some('i') => {
+            assert_eq!(encoded_value.chars().last(), Some('e'), "bencoded integer doesn't end with e");
+            let numerals = &encoded_value[1..encoded_value.len()-1];
+            let integer: isize = numerals.parse().expect("failed to parse numerals into integer");
+            serde_json::Value::Number(integer.into())
+        }
+        Some(c) if c.is_ascii_digit() => {
+            // Example: "5:hello" -> "hello"
+            let colon_index = encoded_value.find(':').unwrap();
+            let number_string = &encoded_value[..colon_index];
+            let number = number_string.parse::<i64>().unwrap();
+            let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+            serde_json::Value::String(string.to_string())
+        }
+        Some(_) | None => {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        }
     }
 }
 
@@ -28,7 +36,7 @@ fn main() {
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        println!("{}", decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }
